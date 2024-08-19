7c6feb002f6892975298ea3b866be045ae50f431
Miiashiro <miiashiro.caio1@gmail.com>
2024-08-19 18:57:01 -0300

    adicionando função aos botões

--- js/script.js
+++ js/script.js
@@ -3,22 +3,39 @@ const pokemonNumber = document.querySelector('.pokemon_number');
 const pokemonImage = document.querySelector('.pokemon_image');
 const form = document.querySelector('.form')
 const input = document.querySelector('.input_search')
+const buttonPrev = document.querySelector('.btn-prev')
+const buttonNext = document.querySelector('.btn-next')
+
+let searchPokemonNumber = 1
 
 const fetchPokemon = async (pokemon) => {
     const apiPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
-    const data = await apiPokemon.json();
+    if (apiPokemon.status === 200) {
+        const data = await apiPokemon.json();
 
-    return data;
+        return data;
+    }
 }
 
 const renderPokemon = async (pokemon) => {
+    pokemonName.innerHTML = "Carregando...";
+    
     const data = await fetchPokemon(pokemon);
 
-    pokemonName.innerHTML = data.name;
-    pokemonNumber.innerHTML = data.id;
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']
-    ['front_default']
+    if (data) {
+        pokemonImage.style.display = 'block';
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']
+        ['front_default'];
+        searchPokemonNumber = data.id;
+    } else {
+        pokemonName.innerHTML = 'Pokemon não encontrado';
+        pokemonNumber.innerHTML = '';
+        pokemonName.style.fontSize = '21px';
+        pokemonImage.style.display = 'none';
+    }
 }
 
 form.addEventListener('submit', (event) => {
@@ -28,3 +45,17 @@ form.addEventListener('submit', (event) => {
 
     input.innerHTML = '';
 })
+
+buttonPrev.addEventListener('click', () => {    
+    if(searchPokemonNumber > 1){
+        searchPokemonNumber -= 1;
+        renderPokemon(searchPokemonNumber);
+    }
+})
+
+buttonNext.addEventListener('click', () => {
+    searchPokemonNumber += 1;
+    renderPokemon(searchPokemonNumber);
+})
+
+renderPokemon(searchPokemonNumber)
